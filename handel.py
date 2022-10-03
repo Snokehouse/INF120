3aa88e9767a033159465424db347450b882e662e
Sverre Larsen <schlarsen@outlook.com>
2022-10-03 18:30:51 +0200

    Mer beskrivende Kommentarer

--- handel.py
+++ handel.py
@@ -3,7 +3,7 @@
 __author__ = "Maria Nybo"
 __email__ = "brukenavn@nmbu.no"
 
-#Funksjon for å "vaske" input slik at det kun er mulig å registrere tall
+#Funksjon for å "vaske" input slik at det kun er mulig å registrere tall.
 def check_input(type):
     while True:
         try:
@@ -14,23 +14,25 @@ def check_input(type):
     return output
 
 #Funksjon for å registrere Vare, antall og prisen per vare.
-#Benytter en rekursiv funksjon istedet for å bruke en loop for å legge til så mange varer man skulle ønske
-#Funksjonen returnerer en liste med varer
+#Benytter en rekursiv funksjon istedet for å bruke en loop for å legge til så mange varer man skulle ønske.
+#Funksjonen returnerer en liste med varer.
 def innlesning(vareliste = []):
     print("Registrer ditt input, registrer blankt for å avslutte.")
     varenavn = input("Vare beskrivelse: ")
     #Lagt inn muligheten til å avslutte funksjonen tidlig om brukeren ikke har flere varer å legge inn.
     if not varenavn:
         return vareliste
+    #Input vil her bli sjekket om det er en float før det blir lagret i variablen.(Funksjonen returnerer korekt input om det blir skrevet inn en float.)
     antall = check_input("Antall")
     stykkpris = check_input("Pris")
+    #Oppdaterer varelisten og legger inn fra brukerinput.
     innlest_verdi = tuple((varenavn, antall, stykkpris))
     vareliste.append(innlest_verdi)
     #Her vil funksjonen fortsette videre for å legge til neste vare ved å starte funksjonen på nytt. Vi tar vare på "varelisten" så den ikke blir borte i den rekursive funksjonen.
     output = innlesning(vareliste)
     return output
 
-#funksjon for å bearbeide utskrift av listen med varer
+#funksjon for å bearbeide utskrift av listen med varer.
 def utskrift(vareliste):
     total_sum = 0
     print("{:<15}{:>15}".format("Beskrivelse", "Linjekost"))
